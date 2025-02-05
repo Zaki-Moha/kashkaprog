188e586100cdb48cb1f3c8c9dde316ec79730f2b
Zakarie Mohamed <zakimohaze@gmail.com>
2025-02-05 16:15:36 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,7 @@
+// Dark Mode Toggle
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
